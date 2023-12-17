08a9a42c02ead788db833985b10e2339df514bdf
jackalot <68657425+jackalot@users.noreply.github.com>
2023-12-17 16:05:35 -0500

    changed our input values to be seperate
    Changed our input values to be seperated, so we can later validate them.

--- src/Start.java
+++ src/Start.java
@@ -2,7 +2,8 @@ import javax.swing.JOptionPane;
 public class Start {
 	public static void main(String[] args)
 	{
-		int num = Integer.parseInt(JOptionPane.showInputDialog("Please insert a number"));
+		String input = JOptionPane.showInputDialog("Please insert a number greater than 1");
+		int num = Integer.parseInt(input);
 		System.out.print(num);
 	}
 }
