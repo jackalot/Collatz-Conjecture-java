88c1f872843498678495e72b232d9ba5174005ec
jackalot <68657425+jackalot@users.noreply.github.com>
2023-12-17 16:12:00 -0500

    Start now checks for valid input

--- src/Start.java
+++ src/Start.java
@@ -2,7 +2,13 @@ import javax.swing.JOptionPane;
 public class Start {
 	public static void main(String[] args)
 	{
-		String input = JOptionPane.showInputDialog("Please insert a number greater than 1");
+		String input = JOptionPane.showInputDialog("Please insert an integer greater than 1");
+		if(input == null || Integer.parseInt(input) < 2)
+		{
+			int randomNum = (int) (Math.random() * 99) + 1;
+			input = "";
+			input += randomNum;
+		}
 		int num = Integer.parseInt(input);
 		System.out.print(num);
 	}
