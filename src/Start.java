eba201c9fca80a720dffb1442694f60a9d6198af
jackalot <68657425+jackalot@users.noreply.github.com>
2023-12-17 15:57:19 -0500

    Initial commit

---
+++ src/Start.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+public class Start {
+	public static void main(String[] args)
+	{
+		int num = Integer.parseInt(JOptionPane.showInputDialog("Please insert a number"));
+		System.out.print(num);
+	}
+}
